1f4682b2ebc331a62976bc379100443ee3f30d6f
Caleb Heath <caleb.heath@gmail.com>
2019-09-18 16:56:50 -0400

    began medium 2

---
+++ RB101-109-Small_Problems/Medium_2/03_Lettercase_Percentage_Ratio.rb
@@ -0,0 +1,8 @@
+def letter_percentages(string)
+
+end
+
+
+p letter_percentages('abCdef 123') == { lowercase: 50, uppercase: 10, neither: 40 }
+p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25 }
+p letter_percentages('123') == { lowercase: 0, uppercase: 0, neither: 100 }
