e7f3969bf9e15f84ad765f0d172b7179254c18fc
caleblayneheath <caleb.heath@gmail.com>
2019-12-02 15:53:19 -0500

    beginning stack machine oo exercise

---
+++ RB120-Object_Oriented_Programming/Medium1/05_Stack_Machine.rb
@@ -0,0 +1,46 @@
+class Minilang
+  def initialize(input_string)
+    @input_string = input_string
+  end
+
+  def PRINT
+    puts @input_string + ' sucka!'
+  end
+
+  def eval
+    send(@input_string)
+  end
+end
+
+Minilang.new('PRINT').eval
+# 0
+
+# Minilang.new('5 PUSH 3 MULT PRINT').eval
+# # 15
+
+# Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
+# # 5
+# # 3
+# # 8
+
+# Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
+# # 10
+# # 5
+
+# Minilang.new('5 PUSH POP POP PRINT').eval
+# # Empty stack!
+
+# Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
+# # 6
+
+# Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
+# # 12
+
+# Minilang.new('-3 PUSH 5 XSUB PRINT').eval
+# # Invalid token: XSUB
+
+# Minilang.new('-3 PUSH 5 SUB PRINT').eval
+# # 8
+
+# Minilang.new('6 PUSH').eval
+# # (nothing printed; no PRINT commands)
