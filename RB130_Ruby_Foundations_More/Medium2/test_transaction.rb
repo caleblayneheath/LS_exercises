0d3b9e68ed08b53f96025db576eed0f5f04dfc14
caleblayneheath <caleb.heath@gmail.com>
2020-01-29 14:45:16 -0500

    finished med2 rb130

--- RB130_Ruby_Foundations_More/Medium2/test_transaction.rb
+++ RB130_Ruby_Foundations_More/Medium2/test_transaction.rb
@@ -7,13 +7,28 @@ require_relative 'transaction.rb'
 class TransactionTest < Minitest::Test
   def test_prompt_for_payment
     transaction = Transaction.new(30)
-    io = StringIO.new('30')
-    transaction.prompt_for_payment(input: input)
+
+    # input = StringIO.new("-20\n10\n30\n")
+    input = StringIO.new('30\n')
+    
+    # not being used
+    output = <<~OUTPUT.chomp
+      You owe $30.
+      How much are you paying?
+      That is not the correct amount. Please make sure to pay the full cost.
+      You owe $30.
+      How much are you paying?
+      That is not the correct amount. Please make sure to pay the full cost.
+      You owe $30.
+      How much are you paying?
+
+    OUTPUT
+
+    # assert_output(output) { transaction.prompt_for_payment(input: input) }
+    out = StringIO.new
+    transaction.prompt_for_payment(input: input, output: out)
     assert_equal(30, transaction.amount_paid)
 
-    # expected_output = 'That is not the correct amount. \nPlease make sure to pay the full cost.\n'
-    # io = StringIO.new('2\n')
-    # assert_output(expected_output) { transaction.prompt_for_payment(input:io)}
   end
   
 end
