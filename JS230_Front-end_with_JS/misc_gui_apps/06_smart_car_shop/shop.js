9766194f4c1aefc8f5b6fc722b17f27da12c13ca
caleblayneheath <caleb.heath@gmail.com>
2021-04-06 15:57:20 -0400

    finished stopwatch

--- JS230_Front-end_with_JS/misc_gui_apps/06_smart_car_shop/shop.js
+++ JS230_Front-end_with_JS/misc_gui_apps/06_smart_car_shop/shop.js
@@ -1,7 +1,7 @@
 const cars = [
   { make: 'Honda', image: 'images/honda-accord-2005.jpg', model: 'Accord', year: 2005, price: 7000 },
   { make: 'Honda', image: 'images/honda-accord-2008.jpg', model: 'Accord', year: 2008, price: 11000 },
-  { make: 'Toyota', image: 'images/toyota-camry-2009.jpg', model: 'Camry', year: 2009, price: 12500 },
+  { make: 'Toyota', image: 'images/toyota-camry-2009.jpg', model: 'Camry', year: 2005, price: 12500 },
   { make: 'Toyota', image: 'images/toyota-corrolla-2016.jpg', model: 'Corolla', year: 2016, price: 15000 },
   { make: 'Suzuki', image: 'images/suzuki-swift-2014.jpg', model: 'Swift', year: 2014, price: 9000 },
   { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: 2013, price: 25000 },
@@ -14,44 +14,73 @@ let getSelections = (list, key) => [...new Set(list.map(elem => elem[key]))];
 
 document.addEventListener('DOMContentLoaded', () => {
   let carTemplate = Handlebars.compile(document.querySelector('#car_template').innerHTML);
+  let filtersTemplate = Handlebars.compile(document.querySelector('#filters_template').innerHTML);
   let carList = document.querySelector('#listings');
+  let searchForm = document.querySelector('#search');
+  let filters = document.querySelector('#filters');
+  let validCars = [...cars];
 
-  cars.forEach(car => carList.insertAdjacentHTML('beforeend', carTemplate(car)));
+  let loadListings = () => {
+    carList.replaceChildren()
+    validCars.forEach(car => carList.insertAdjacentHTML('beforeend', carTemplate(car)));
+  };
 
-  filterKeys.forEach(filterKey => {
-    let values = getSelections(cars, filterKey);
+  let loadFilters = () => {
+    filters.replaceChildren();
+    filters.insertAdjacentHTML('afterbegin', filtersTemplate());
 
-    if (typeof(values[0]) === 'number') {
-      values = values.sort((a, b) => a - b);
-    } else {
-      values = values.sort();
-    }
+    filterKeys.forEach(filterKey => {
+      let values = getSelections(validCars, filterKey);
 
-    let selector = document.querySelector(`#${filterKey}`);
-    values.forEach(value => {
-      selector.insertAdjacentHTML('beforeend', `<option value="${value}">${value}</option`);
-    }); 
-  });
-
-  filters.addEventListener('submit', event => {
-    event.preventDefault();
-
-    let query = {};
-    for (let [key, value] of new FormData(filters)) {
-      if (value) {
-        query[key] = value;
+      if (typeof(values[0]) === 'number') {
+        values = values.sort((a, b) => a - b);
+      } else {
+        values = values.sort();
       }
+
+      let selector = document.querySelector(`#${filterKey}`);
+      values.forEach(value => {
+        selector.insertAdjacentHTML('beforeend', `<option value="${value}">${value}</option`);
+      }); 
+    });
+  };
+
+  loadFilters();
+  loadListings();  
+
+  filters.addEventListener('input', event => {
+    let selections = {};
+    filterKeys.forEach(key => {
+      let value = filters.querySelector(`#${key}`).value;
+      selections[key] = value;
+    });
+
+    let key = event.target.name;
+    let value = event.target.value;
+    
+    if (value.length > 0) {
+      validCars = validCars.filter(car => car[key].toString() === value);
     }
     
-    let searchTerms = Object.keys(query);
+    loadFilters();
 
-    [...listings.children].forEach(listing => {
-      let data = listing.dataset;
-      if (searchTerms.every(term => query[term] === data[term])) {
-        listing.classList.add('match');
-      } else {
-        listing.classList.remove('match');
-      }
+    filterKeys.forEach(key => {
+      let value = selections[key];
+      let selector = document.querySelector(`[name ="${key}"]`);
+      let option = selector.querySelector(`[value="${selections[key]}"]`);
+      option.selected = true;
     });
+  });
+
+  search.addEventListener('submit', event => {
+    event.preventDefault();
+    loadListings();
+  });
+
+  search.addEventListener('reset', event => {
+    event.preventDefault();
+    validCars = [...cars];
+    loadFilters();
+    loadListings();
   });
 });
